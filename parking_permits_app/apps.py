220ed9ca470d9ec9b4f570a76c829c823020e246
Sam Hosseini <git-email@hosseini.io>
2021-04-14 13:30:28 +0300

    update parking_permits_app name in project settings

--- parking_permits_app/apps.py
+++ parking_permits_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GraphqlAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'graphql_app'
+    name = 'parking_permits_app'
