971250cff7b4a498ddef1d66bfde33faa40dcd2b
Sam Hosseini <git-email@hosseini.io>
2021-04-16 11:07:16 +0300

    update parking permits app config name

--- parking_permits_app/apps.py
+++ parking_permits_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GraphqlAppConfig(AppConfig):
+class ParkingPermitsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'parking_permits_app'
