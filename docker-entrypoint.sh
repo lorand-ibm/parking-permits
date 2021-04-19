5f5bf089f312805e7a067e495434dca501033f9e
Sam Hosseini <git-email@hosseini.io>
2021-04-19 15:16:54 +0300

    add wait-for-it.sh so django could wait until postgres is initialized

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [[ "$WAIT_FOR_IT_ADDRESS" ]]; then
+    ./wait-for-it.sh $WAIT_FOR_IT_ADDRESS --timeout=30
+fi
+
 if [[ "$DEV_SERVER" = "true" ]]; then
     python /app/manage.py runserver 0.0.0.0:8000
 else
