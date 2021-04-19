034a3b6d4c932e9e4616dc2bd80325fda246a9bc
Sam Hosseini <git-email@hosseini.io>
2021-04-19 16:11:20 +0300

    apply django migrations automatically when using docker-compose up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,10 @@ if [[ "$WAIT_FOR_IT_ADDRESS" ]]; then
     ./wait-for-it.sh $WAIT_FOR_IT_ADDRESS --timeout=30
 fi
 
+if [[ "$APPLY_MIGRATIONS" = "true" ]]; then
+    python /app/manage.py migrate --noinput
+fi
+
 if [[ "$DEV_SERVER" = "true" ]]; then
     python /app/manage.py runserver 0.0.0.0:8000
 else
