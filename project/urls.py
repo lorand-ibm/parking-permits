1adb88a38331492e7f88530900d0a789eb45cdc4
Sam Hosseini <git-email@hosseini.io>
2021-05-12 16:46:49 +0300

    remove redirecting root url to /admin/

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="/admin/")),
     path("", include("parking_permits_app.urls")),
 ]
