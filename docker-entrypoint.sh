93e6fb6217a40e7a52622f380adff042b7e4d35f
Mika Hietanen <m.hietanen84@gmail.com>
2021-05-27 14:53:44 +0300

    Adapt project to use RedHat base image with GDAL 3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,19 +6,19 @@ if [[ "$WAIT_FOR_IT_ADDRESS" ]]; then
     ./wait-for-it.sh $WAIT_FOR_IT_ADDRESS --timeout=30
 fi
 
-if [[ "$APPLY_MIGRATIONS" = "true" ]]; then
+if [[ "$APPLY_MIGRATIONS" = "True" ]]; then
     python /app/manage.py migrate --noinput
 fi
 
-if [[ "$INSTALL_PRECOMMIT" = "true" ]]; then
+if [[ "$INSTALL_PRECOMMIT" = "True" ]]; then
     pre-commit install --overwrite
 fi
 
-if [[ "$CREATE_SUPERUSER" = "true" ]]; then
+if [[ "$CREATE_SUPERUSER" = "True" ]]; then
     python /app/manage.py createsuperuser --noinput || true
 fi
 
-if [[ "$DEV_SERVER" = "true" ]]; then
+if [[ "$DEV_SERVER" = "True" ]]; then
     python /app/manage.py runserver 0.0.0.0:8000
 else
     gunicorn
