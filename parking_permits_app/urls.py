81224199d0ac97d5c25d6ceb751f91539ab505b7
Sam Hosseini <git-email@hosseini.io>
2021-06-10 17:24:26 +0300

    add resolve availability endpoint for TALPA integration

--- parking_permits_app/urls.py
+++ parking_permits_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from parking_permits_app import graphql
+from parking_permits_app import graphql, views
 
 urlpatterns = [
     path("", graphql.view, name="graphql"),
+    path(
+        "api/talpa/resolve-availability/",
+        views.TalpaResolveAvailability.as_view(),
+        name="talpa-availability",
+    ),
 ]
