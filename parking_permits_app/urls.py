b75a2e64c7ece295a59edf323ca827990a5a349a
Sam Hosseini <git-email@hosseini.io>
2021-06-10 17:30:52 +0300

    Revert "update root url to go directly to graphql playground"
    This reverts commit a8160840d22925d4c7597b452f5839901b0433e7.

--- parking_permits_app/urls.py
+++ parking_permits_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from parking_permits_app import graphql, views
 
 urlpatterns = [
-    path("", graphql.view, name="graphql"),
+    path("graphql/", graphql.view, name="graphql"),
     path(
         "api/talpa/resolve-availability/",
         views.TalpaResolveAvailability.as_view(),
