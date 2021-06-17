ce3ada69e1d2e4f9cdc0bbf455d0a521ce49ccc9
Aman Yadav <amanpdyadav@gmail.com>
2021-06-17 10:02:48 +0300

    Add versioned API endpoint for cart
    Refs PV-135

--- parking_permits_app/urls.py
+++ parking_permits_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from parking_permits_app import graphql, views
 
+from .url_utils import versioned_url
+
+router = DefaultRouter()
+router.register(r"cart", views.ProductViewSet, basename="cart")
+
+app_name = "parking_permit"
 urlpatterns = [
     path("graphql/", graphql.view, name="graphql"),
     path(
@@ -9,4 +16,5 @@ urlpatterns = [
         views.TalpaResolveAvailability.as_view(),
         name="talpa-availability",
     ),
+    versioned_url("v1", router.urls),
 ]
