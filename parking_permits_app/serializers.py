fe88c4b71e0c12e9078e294bdab665c6f6e7c4cb
Aman Yadav <amanpdyadav@gmail.com>
2021-06-22 09:18:20 +0300

    Add serializer for product price model
    Refs PV-135

--- parking_permits_app/serializers.py
+++ parking_permits_app/serializers.py
@@ -1,18 +1,32 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, ProductPrice
+
+
+class ProductPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductPrice
+        fields = [
+            "id",
+            "price",
+            "start_date",
+            "end_date",
+        ]
+        read_only_fields = [
+            "id",
+        ]
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    prices = ProductPriceSerializer(read_only=True, many=True)
+
     class Meta:
         model = Product
         fields = [
             "id",
             "shared_product_id",
             "name",
-            "price",
-            "start_date",
-            "end_date",
+            "prices",
         ]
         read_only_fields = [
             "id",
