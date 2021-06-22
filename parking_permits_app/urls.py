74611dfcb80fd212eeb175918404789092c59c89
Aman Yadav <amanpdyadav@gmail.com>
2021-06-22 09:23:01 +0300

    Update endpoint from cart to product
    Cart endpoints are handled by TALPA and
    therefore there wont be any endpoint about
    cart on parking permit side.
    
    Refs PV-135

--- parking_permits_app/urls.py
+++ parking_permits_app/urls.py
@@ -6,7 +6,7 @@ from parking_permits_app import graphql, views
 from .url_utils import versioned_url
 
 router = DefaultRouter()
-router.register(r"cart", views.ProductViewSet, basename="cart")
+router.register(r"product", views.ProductViewSet, basename="product")
 
 app_name = "parking_permits_app"
 urlpatterns = [
