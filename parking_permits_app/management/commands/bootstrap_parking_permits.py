1adbc10433e46547e89a38d2ea97c58d11e413ca
Sam Hosseini <git-email@hosseini.io>
2021-07-01 22:59:56 +0300

    add bootstrap_parking_permits command

---
+++ parking_permits_app/management/commands/bootstrap_parking_permits.py
@@ -0,0 +1,23 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Usage: python manage.py bootstrap_parking_permits"
+
+    def handle(self, *args, **options):
+        call_command("create_products")
+
+        call_command("create_vehicle_types")
+
+        call_command(
+            "create_products_prices",
+            year=2021,
+            data_module="parking_permits_app.data.2021",
+        )
+
+        call_command(
+            "create_low_emission_criteria",
+            year=2021,
+            data_module="parking_permits_app.data.2021",
+        )
