dd8ca95c5942f28b0a1dfdd3b9129700ef349ede
Sam Hosseini <git-email@hosseini.io>
2021-07-16 11:26:52 +0300

    add resolve_price_response helper function for talpa

--- parking_permits_app/services/talpa.py
+++ parking_permits_app/services/talpa.py
@@ -1,3 +1,7 @@
+from parking_permits_app.constants import VAT_PERCENTAGE
+from parking_permits_app.pricing.value_added_tax import calculate_price_without_vat
+
+
 def get_meta_value(meta_pair_list, meta_pair_key):
     return next(
         (
@@ -7,3 +11,13 @@ def get_meta_value(meta_pair_list, meta_pair_key):
         ),
         None,
     )
+
+
+def resolve_price_response(product_id=None, total_price=None):
+    return {
+        "productId": product_id,
+        "netValue": calculate_price_without_vat(total_price),
+        "vatPercentage": VAT_PERCENTAGE,
+        "grossValue": total_price,
+        "vatValue": total_price - calculate_price_without_vat(total_price),
+    }
