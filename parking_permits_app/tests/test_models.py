9e90e2bb98174068f903e0739e2e8ab90d2f4aa8
Sam Hosseini <git-email@hosseini.io>
2021-08-03 11:08:49 +0300

    fix all formatting issues

--- parking_permits_app/tests/test_models.py
+++ parking_permits_app/tests/test_models.py
@@ -11,7 +11,7 @@ def test_product_prices(product_factory, product_price_factory):
         start_date=datetime.datetime(2014, 1, 1, 6, 0, 0, tzinfo=utc),
         end_date=datetime.datetime(2016, 1, 1, 7, 0, 0, tzinfo=utc),
         product=product,
-        price=20.0
+        price=20.0,
     )
     assert str(product_price.start_date).count("2014") == 1
     assert str(product_price.end_date).count("2016") == 1
@@ -21,7 +21,7 @@ def test_product_prices(product_factory, product_price_factory):
         start_date=datetime.datetime(2016, 1, 1, 6, 0, 0, tzinfo=utc),
         end_date=None,
         product=product,
-        price=33.0
+        price=33.0,
     )
     assert str(product_price_open_end.start_date).count("2016") == 1
     assert product_price_open_end.end_date is None
