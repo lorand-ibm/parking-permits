d08cfa1e65d25cba2db1c95e8d10f3d68a778ae1
Aman Yadav <amanpdyadav@gmail.com>
2021-08-06 11:16:04 +0300

    Update product model ref with Parking zone
    Refs 140

--- parking_permits_app/models/parking_permit.py
+++ parking_permits_app/models/parking_permit.py
@@ -5,7 +5,6 @@ from .contract_type import ContractType
 from .customer import Customer
 from .mixins import TimestampedModelMixin, UUIDPrimaryKeyMixin
 from .parking_zone import ParkingZone
-from .product import Product
 from .vehicle import Vehicle
 
 
@@ -20,13 +19,6 @@ class ParkingPermit(TimestampedModelMixin, UUIDPrimaryKeyMixin):
     vehicle = models.ForeignKey(
         Vehicle,
         verbose_name=_("Vehicle"),
-        on_delete=models.PROTECT,
-        blank=False,
-        null=False,
-    )
-    product = models.ForeignKey(
-        Product,
-        verbose_name=_("Product"),
         on_delete=models.PROTECT,
         blank=False,
         null=False,
@@ -54,11 +46,10 @@ class ParkingPermit(TimestampedModelMixin, UUIDPrimaryKeyMixin):
         verbose_name_plural = _("Parking permits")
 
     def __str__(self):
-        return "%s - %s - %s - %s - %s - %s - %s" % (
+        return "%s - %s - %s - %s - %s - %s" % (
             self.id,
             self.customer,
             self.vehicle,
-            self.product,
             self.contract_type,
             self.start_time,
             self.end_time,
--- parking_permits_app/models/price.py
+++ parking_permits_app/models/price.py
@@ -1,21 +1,21 @@
 from django.contrib.gis.db import models
 from django.utils.translation import gettext_lazy as _
 
-from . import Product
 from .mixins import TimestampedModelMixin, UUIDPrimaryKeyMixin
+from .parking_zone import ParkingZone
 
 
-class ProductPrice(TimestampedModelMixin, UUIDPrimaryKeyMixin):
-    product = models.ForeignKey(
-        Product,
-        verbose_name=_("Product price"),
+class Price(TimestampedModelMixin, UUIDPrimaryKeyMixin):
+    zone = models.ForeignKey(
+        ParkingZone,
+        verbose_name=_("Zone"),
         on_delete=models.PROTECT,
         related_name="prices",
         blank=True,
         null=True,
     )
     price = models.DecimalField(
-        _("Product price"), blank=False, null=False, max_digits=6, decimal_places=2
+        _("Price"), blank=False, null=False, max_digits=6, decimal_places=2
     )
     start_date = models.DateField(_("Start date"), blank=False, null=False)
     end_date = models.DateField(_("End date"), blank=True, null=True)
