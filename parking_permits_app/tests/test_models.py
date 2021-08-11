1f1c27d3eeea82d93a8d76ba6b7a4a68bf2439bd
Aman Yadav <amanpdyadav@gmail.com>
2021-08-11 12:41:37 +0300

    Update the tests from Product to ParkingZone
    Refs 179

--- parking_permits_app/tests/test_models.py
+++ parking_permits_app/tests/test_models.py
@@ -5,24 +5,24 @@ from django.utils.timezone import utc
 
 
 @pytest.mark.django_db
-def test_product_prices(product_factory, product_price_factory):
-    product = product_factory()
-    product_price = product_price_factory(
+def test_zone_prices(parking_zone_factory, price_factory):
+    zone = parking_zone_factory()
+    zone_price = price_factory(
         start_date=datetime.datetime(2014, 1, 1, 6, 0, 0, tzinfo=utc),
         end_date=datetime.datetime(2016, 1, 1, 7, 0, 0, tzinfo=utc),
-        product=product,
+        zone=zone,
         price=20.0,
     )
-    assert str(product_price.start_date).count("2014") == 1
-    assert str(product_price.end_date).count("2016") == 1
-    assert product_price.price == 20.0
+    assert str(zone_price.start_date).count("2014") == 1
+    assert str(zone_price.end_date).count("2016") == 1
+    assert zone_price.price == 20.0
 
-    product_price_open_end = product_price_factory(
+    zone_price_open_end = price_factory(
         start_date=datetime.datetime(2016, 1, 1, 6, 0, 0, tzinfo=utc),
         end_date=None,
-        product=product,
+        zone=zone,
         price=33.0,
     )
-    assert str(product_price_open_end.start_date).count("2016") == 1
-    assert product_price_open_end.end_date is None
-    assert product_price_open_end.price is not None
+    assert str(zone_price_open_end.start_date).count("2016") == 1
+    assert zone_price_open_end.end_date is None
+    assert zone_price_open_end.price is not None
