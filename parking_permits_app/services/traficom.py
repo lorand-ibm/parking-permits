94c32b17036f427dbe461ba49215af9f43a17a16
Sam Hosseini <git-email@hosseini.io>
2021-08-20 08:57:52 +0200

    add mock function of get_vehicle_information in traficom.py

---
+++ parking_permits_app/services/traficom.py
@@ -0,0 +1,12 @@
+def get_vehicle_information(registration_number):
+    return {
+        "type": "Bensin",
+        "category": "M1",
+        "manufacturer": "Tesla",
+        "model": "Supreme",
+        "production_year": 2000,
+        "euro_class": 6,
+        "emission": 120,
+        "emission_type": "WLTP",
+        "last_inspection_date": "2022-08-13",
+    }
