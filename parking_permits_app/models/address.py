b8e148a5aa6c79fb381f3ccf0420edc77a8f44cd
Aman Yadav <amanpdyadav@gmail.com>
2021-08-30 10:43:59 +0300

    Update address model
    * Add street_name_sv filed
    * Use same id as helsinki for address
    * Allow location to be null and blank(
      for incorrect address)
    * Override save method to do the KMO stuff
    
    Refs PV-164

--- parking_permits_app/models/address.py
+++ parking_permits_app/models/address.py
@@ -1,21 +1,27 @@
 from django.conf import settings
 from django.contrib.gis.db import models
+from django.contrib.gis.geos import GEOSGeometry
 from django.utils.translation import gettext_lazy as _
 
-from .mixins import TimestampedModelMixin, UUIDPrimaryKeyMixin
+from ..services.kmo import get_wfs_result
+from .mixins import TimestampedModelMixin
 from .parking_zone import ParkingZone
 
 
-class Address(TimestampedModelMixin, UUIDPrimaryKeyMixin):
+class Address(TimestampedModelMixin):
+    id = models.TextField(primary_key=True, unique=True, editable=False)
     street_name = models.CharField(
         _("Street name"), max_length=128, blank=False, null=False
+    )
+    street_name_sv = models.CharField(
+        _("Street name sv"), max_length=128, blank=False, null=False, default=""
     )
     street_number = models.CharField(
         _("Street number"), max_length=128, blank=False, null=False
     )
     city = models.CharField(_("City"), max_length=128, blank=False, null=False)
     location = models.PointField(
-        _("Location (2D)"), srid=settings.SRID, blank=False, null=False
+        _("Location (2D)"), srid=settings.SRID, blank=True, null=True
     )
     zone = models.ForeignKey(
         ParkingZone,
@@ -37,13 +43,32 @@ class Address(TimestampedModelMixin, UUIDPrimaryKeyMixin):
             self.city,
         )
 
-    def get_zone(self):
+    def convert_to_geosgeometry(self, geometry):
+        return GEOSGeometry(str(geometry))
+
+    def get_kmo_features(self):
         if not self.location:
-            return None
-        return ParkingZone.objects.filter(location__intersects=self.location).first()
+            results = get_wfs_result(self.street_name, self.street_number)
+            address_feature = next(
+                feature
+                for feature in results.get("features")
+                if feature.get("geometry").get("type") == "Point"
+            )
+            address_property = address_feature.get("properties")
+            location = self.convert_to_geosgeometry(address_feature.get("geometry"))
+            zone = ParkingZone.objects.filter(location__intersects=location).first()
+            return address_property.get("gatan"), location, zone
+        return self.street_name_sv, self.location, self.zone
 
     def save(self, update_fields=None, *args, **kwargs):
-        if update_fields is None or "zone" in update_fields:
-            self.zone = self.get_zone()
+        if (
+            update_fields is None
+            or "street_name" in update_fields
+            or "street_number" in update_fields
+        ):
+            street_name_sv, location, zone = self.get_kmo_features()
+            self.street_name_sv = street_name_sv
+            self.location = location
+            self.zone = zone
 
         super().save(update_fields=update_fields, *args, **kwargs)
