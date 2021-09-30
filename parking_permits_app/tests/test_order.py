99bd423d7744cbdda627891be37ad57349424915
Mingfeng Yu <mfdevworld@gmail.com>
2021-09-30 12:48:23 +0300

    Minor refactoring on tests

--- parking_permits_app/tests/test_order.py
+++ parking_permits_app/tests/test_order.py
@@ -1,21 +1,17 @@
-import pytest
+from rest_framework.test import APIClient
 
 from .utils import post
 
-order_url = "/api/talpa/order/"
 
-
-@pytest.fixture
-def new_order_data():
-    return {
+def test_post_order():
+    api_client = APIClient()
+    order_url = "/api/talpa/order/"
+    new_order_data = {
         "userId": "UHJvZmlsZU5vZGU6NTI5MjFmZTctOWRjYS00ZmYyLWE4MjgtN",
         "permitId": "ZGU6NTI5MjFmZTctOWRjYS00ZmYyLWE4MjgtN",
         "start_time": "2020-01-01T00:00:00Z",
         "end_time": "2021-12-31T23:59:00Z",
         "status": "VALID",
     }
-
-
-def test_post_order(api_client, new_order_data):
     response = post(api_client, order_url, new_order_data, 200)
     assert response.get("message") is not None
