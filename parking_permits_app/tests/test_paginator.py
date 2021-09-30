78dd4b3bcf22daede9017934c7a59acf05a4adfe
Mingfeng Yu <mfdevworld@gmail.com>
2021-09-30 10:59:10 +0300

    Add a queryset paginator
    We need to paginate the permits for the admin front page
    
    Refs: PV-213

---
+++ parking_permits_app/tests/test_paginator.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from parking_permits_app.factories.parking_permit import ParkingPermitFactory
+from parking_permits_app.models import ParkingPermit
+from parking_permits_app.paginator import QuerySetPaginator
+
+
+class QuerySetPaginatorTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        for i in range(10):
+            ParkingPermitFactory()
+
+    def test_paginator_with_default_page_size(self):
+        qs = ParkingPermit.objects.all()
+        page_input = {"page": 1}
+        paginator = QuerySetPaginator(qs, page_input)
+        self.assertEqual(paginator.object_list.count(), 10)
+        expected_page_info = {"num_pages": 1, "next": None, "prev": None}
+        self.assertEqual(paginator.page_info, expected_page_info)
+
+    def test_paginator_with_custom_page_size(self):
+        qs = ParkingPermit.objects.all()
+        page_input = {"page": 2, "page_size": 3}
+        paginator = QuerySetPaginator(qs, page_input)
+        self.assertEqual(paginator.object_list.count(), 3)
+        expected_page_info = {"num_pages": 4, "next": 3, "prev": 1}
+        self.assertEqual(paginator.page_info, expected_page_info)
