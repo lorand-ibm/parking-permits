566b508aeb7c656ce5eb2156b1e783ca385b6f77
Mingfeng Yu <mfdevworld@gmail.com>
2021-10-11 14:29:00 +0300

    Remove ordering from paginator
    The queryset ordering should not be part of paginator
    
    no refs

--- parking_permits_app/paginator.py
+++ parking_permits_app/paginator.py
@@ -5,12 +5,6 @@ class QuerySetPaginator:
     default_page_size = 10
 
     def __init__(self, qs, page_input):
-        order_by = page_input.get("order_by")
-        order_direction = page_input.get("order_direction")
-        if order_by and order_direction == "desc":
-            order_by = f"-{order_by}"
-        if order_by:
-            qs = qs.order_by(order_by)
         page_size = page_input.get("page_size", self.default_page_size)
         self.paginator = Paginator(qs, page_size)
         page_index = page_input.get("page", 1)
