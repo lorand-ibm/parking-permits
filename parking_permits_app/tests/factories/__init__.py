1c834f062d24e1c298bcdd2bb4b67f8c96218f35
Aman Yadav <amanpdyadav@gmail.com>
2021-10-28 23:36:45 +0300

    Add LowEmissionCriteriaFactory
    Refs PV-270

--- parking_permits_app/tests/factories/__init__.py
+++ parking_permits_app/tests/factories/__init__.py
@@ -1,7 +1,5 @@
+from .low_emission_criteria import LowEmissionCriteriaFactory
 from .price import PriceFactory
 from .zone import ParkingZoneFactory
 
-__all__ = [
-    "PriceFactory",
-    "ParkingZoneFactory",
-]
+__all__ = ["PriceFactory", "ParkingZoneFactory", "LowEmissionCriteriaFactory"]
