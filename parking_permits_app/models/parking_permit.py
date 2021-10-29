4c46ca0bc285be6962428a8a9a0389d98ffa7be9
Aman Yadav <amanpdyadav@gmail.com>
2021-10-29 12:11:54 +0300

    Use timezone as default for dattime field
    While creating a parking permit it use to throw error
    regarding naive date time as time zone support is active.
    
    Refs PV-270

--- parking_permits_app/models/parking_permit.py
+++ parking_permits_app/models/parking_permit.py
@@ -1,9 +1,9 @@
 import decimal
-from datetime import datetime
 
 import reversion
 from django.contrib.gis.db import models
 from django.utils import timezone
+from django.utils import timezone as tz
 from django.utils.translation import gettext_lazy as _
 
 from .. import constants
@@ -59,7 +59,7 @@ class ParkingPermit(TimestampedModelMixin, UUIDPrimaryKeyMixin):
         default=get_next_identifier, editable=False, unique=True, db_index=True
     )
     consent_low_emission_accepted = models.BooleanField(null=False, default=False)
-    start_time = models.DateTimeField(_("Start time"), default=datetime.now)
+    start_time = models.DateTimeField(_("Start time"), default=tz.now)
     end_time = models.DateTimeField(_("End time"), blank=True, null=True)
     primary_vehicle = models.BooleanField(null=False, default=True)
     contract_type = models.CharField(
