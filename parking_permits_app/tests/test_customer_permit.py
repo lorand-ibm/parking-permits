e3f1d6865d62217546cccf8b12eaf7a6ad75d7fc
Aman Yadav <amanpdyadav@gmail.com>
2021-10-29 09:15:49 +0300

    Add tests for creating parking permit
    Refs PV-270

--- parking_permits_app/tests/test_customer_permit.py
+++ parking_permits_app/tests/test_customer_permit.py
@@ -3,6 +3,7 @@ from django.utils import timezone as tz
 
 from parking_permits_app import constants
 from parking_permits_app.customer_permit import CustomerPermit
+from parking_permits_app.exceptions import InvalidUserZone, PermitLimitExceeded
 from parking_permits_app.tests.factories import (
     LowEmissionCriteriaFactory,
     ParkingZoneFactory,
@@ -21,6 +22,9 @@ CANCELLED = constants.ParkingPermitStatus.CANCELLED.value
 EXPIRED = constants.ParkingPermitStatus.EXPIRED.value
 PROCESSING = constants.ParkingPermitStatus.PROCESSING.value
 IMMEDIATELY = constants.StartType.IMMEDIATELY.value
+OPEN_ENDED = constants.ContractType.OPEN_ENDED.value
+FIXED_PERIOD = constants.ContractType.FIXED_PERIOD.value
+BENSIN = constants.VehicleType.BENSIN.value
 
 
 def previous_day():
@@ -102,3 +106,85 @@ class GetCustomerPermitTestCase(TestCase):
         )
         permits = CustomerPermit(customer.id).get()
         self.assertEqual(len(permits), 0)
+
+
+class CreateCustomerPermitTestCase(TestCase):
+    def setUp(self):
+        self.customer_a = CustomerFactory(
+            first_name="Firstname A", last_name="Lastname 1"
+        )
+        self.customer_b = CustomerFactory(
+            first_name="Firstname B", last_name="Lastname 2"
+        )
+        self.customer_c = CustomerFactory(
+            first_name="Firstname C", last_name="Lastname 3"
+        )
+        self.customer_a_zone = self.customer_a.primary_address.zone
+        self.vehicle_type = VehicleTypeFactory(type=BENSIN)
+        self.zone = ParkingZoneFactory()
+        self.vehicle_a = VehicleFactory(type=self.vehicle_type)
+        PriceFactory(zone=self.zone)
+        PriceFactory(zone=self.customer_a_zone)
+        LowEmissionCriteriaFactory(vehicle_type=self.vehicle_type)
+        ParkingPermitFactory(
+            customer=self.customer_a,
+            status=DRAFT,
+            primary_vehicle=True,
+            parking_zone=self.customer_a_zone,
+            vehicle=self.vehicle_a,
+        )
+        self.customer_c_valid_primary_permit = ParkingPermitFactory(
+            customer=self.customer_c,
+            status=VALID,
+            primary_vehicle=True,
+            contract_type=FIXED_PERIOD,
+            parking_zone=self.customer_c.primary_address.zone,
+            vehicle=self.vehicle_a,
+        )
+
+    def test_customer_a_can_create_secondary_permit(self):
+        customer_permit = CustomerPermit(self.customer_a.id)
+        self.assertEqual(len(customer_permit.get()), 1)
+        permit = CustomerPermit(self.customer_a.id).create(str(self.customer_a_zone.id))
+        self.assertEqual(len(customer_permit.get()), 2)
+        self.assertEqual(permit.primary_vehicle, False)
+
+    def test_customer_a_can_not_create_permit_in_zone_outside_his_address(self):
+        with self.assertRaisesMessage(InvalidUserZone, "Invalid user zone."):
+            CustomerPermit(self.customer_a.id).create(self.zone.id)
+
+    def test_customer_a_gets_error_for_exceeding_max_2_permit(self):
+        msg = "You can have a max of 2 permits."
+        with self.assertRaisesMessage(PermitLimitExceeded, msg):
+            for i in range(0, 3):
+                CustomerPermit(self.customer_a.id).create(str(self.customer_a_zone.id))
+
+    def test_customer_b_can_not_buy_permit_to_other_zone_if_he_has_any_valid_permit_to_primary_address(
+        self,
+    ):
+        vehicle = VehicleFactory(type=self.vehicle_type)
+        primary_zone = self.customer_b.primary_address.zone
+        ParkingPermitFactory(
+            customer=self.customer_b,
+            status=VALID,
+            primary_vehicle=True,
+            parking_zone=self.customer_b.primary_address.zone,
+            vehicle=vehicle,
+        )
+        other_add_zone = self.customer_b.other_address.zone
+        msg = f"You can buy permit only for zone {primary_zone.name}"
+        with self.assertRaisesMessage(InvalidUserZone, msg):
+            CustomerPermit(self.customer_b.id).create(str(other_add_zone.id))
+
+    def test_customer_b_can_buy_permit_to_same_zone_that_he_has_at_least_one_valid_permit(
+        self,
+    ):
+        primary_zone = self.customer_c.primary_address.zone
+        permit = CustomerPermit(self.customer_c.id).create(str(primary_zone.id))
+        self.assertEqual(
+            permit.contract_type, self.customer_c_valid_primary_permit.contract_type
+        )
+        self.assertEqual(
+            permit.primary_vehicle,
+            not self.customer_c_valid_primary_permit.primary_vehicle,
+        )
