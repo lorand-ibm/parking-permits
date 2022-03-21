5852bd59495f13be3e6a10a4ae4cef7548f021dc
Aman Yadav <amanpdyadav@gmail.com>
2022-03-21 09:11:07 +0200

    Add allowed vehicle classes for a driving licence
    Check the ticket for more details
    https://helsinkisolutionoffice.atlassian.net/jira/software/c/projects/PV/boards/143\?modal\=detail\&selectedIssue\=PV-95
    
    Refs PV-95

--- parking_permits/models/driving_class.py
+++ parking_permits/models/driving_class.py
@@ -2,15 +2,38 @@ from django.contrib.gis.db import models
 from django.utils.translation import gettext_lazy as _
 
 from .mixins import TimestampedModelMixin, UUIDPrimaryKeyMixin
+from .vehicle import VehicleClass
+
+ALLOWED_VEHICLE_CLASSES = {
+    "AM/121": (VehicleClass.L6eB,),
+    "A1": (VehicleClass.L3eA1,),
+    "A2": (VehicleClass.L3eA2,),
+    "A": (
+        VehicleClass.L3eA1,
+        VehicleClass.L3eA2,
+        VehicleClass.L3eA3,
+    ),
+    "B": (
+        VehicleClass.M1,
+        VehicleClass.M2,  # M2 mass less than 3500KG
+        VehicleClass.N1,
+        VehicleClass.L6eB,
+    ),
+    "C": (VehicleClass.N2,),
+    "D": (VehicleClass.M2,),  # M2 mass not more than 3500 - 4000KG
+}
 
 
 class DrivingClass(TimestampedModelMixin, UUIDPrimaryKeyMixin):
-    class_name = models.CharField(_("Driving class name"), max_length=32)
-    identifier = models.CharField(_("Driving class identifier"), max_length=32)
+    identifier = models.CharField(_("Identifier"), max_length=32)
 
     class Meta:
         verbose_name = _("Driving class")
         verbose_name_plural = _("Driving classes")
 
     def __str__(self):
-        return "%s - %s" % (self.class_name, self.identifier)
+        return "%s" % self.identifier
+
+    @property
+    def vehicle_classes(self):
+        return ALLOWED_VEHICLE_CLASSES.get(self.identifier, ())
